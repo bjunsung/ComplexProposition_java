e32e2d2819a1c6017d4700431decfd203b2e8c8d
baejunsungASUS <baejunsung1225@gmail.com>
2025-05-15 04:18:27 +0900

    Implement the ability to customize the order of propositions

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        ComplexProposition cp1 = Factory.createComplexProposition();
-        cp1.printTruthTable();
+        while (true) {
+            ComplexProposition cp = Factory.createComplexProposition();
+            cp.printTruthTable();
+        }
     }
 }
 
